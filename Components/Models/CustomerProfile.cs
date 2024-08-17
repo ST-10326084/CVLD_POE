20517fa4c1fdb2774b914b42288d39ad80117445
Dean Gibson <deangibson03@gmail.com>
2024-08-17 13:34:38 +0200

    Added basic product and profile pages

---
+++ Components/Models/CustomerProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace CVLD_POE.Components.Models
+{
+    public class CustomerProfile
+    {
+        public string CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
